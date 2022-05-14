fb9a9cc3113423e39cd7044d9f07a2b3c7eede01
Di Zhao <zd277607529@gmail.com>
2022-05-14 19:15:42 +1200

    update

--- dassl/modeling/backbone/__init__.py
+++ dassl/modeling/backbone/__init__.py
@@ -1,1 +1,2 @@
-from .build import build_backbone, BACKBONE_REGISTRY   # isort:skip
+from .build import build_backbone, BACKBONE_REGISTRY    # isort:skip
+from .backbone import Backbone                          # isort:skip
