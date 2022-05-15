7da51d116f3d1f74245489c1e0f3b4ad907f9118
Di Zhao <zd277607529@gmail.com>
2022-05-15 22:00:59 +1200

    update

--- dassl/modeling/backbone/__init__.py
+++ dassl/modeling/backbone/__init__.py
@@ -1,2 +1,10 @@
 from .build import build_backbone, BACKBONE_REGISTRY    # isort:skip
 from .backbone import Backbone                          # isort:skip
+
+
+from .resnet import (
+    resnet18, resnet34, resnet50, resnet101, resnet152, resnet18_ms_l1,
+    resnet50_ms_l1, resnet18_ms_l12, resnet50_ms_l12, resnet101_ms_l1,
+    resnet18_ms_l123, resnet50_ms_l123, resnet101_ms_l12, resnet101_ms_l123,
+    
+)
---
+++ dassl/modeling/ops/__init__.py
@@ -0,0 +1,12 @@
+
+
+from .mixup import mixup
+from .efdmix import (
+    EFDMix
+)
+
+
+
+from .mixstyle import (
+    MixStyle
+)
